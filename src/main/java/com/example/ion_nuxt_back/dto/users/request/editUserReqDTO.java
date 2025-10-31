fbec80d7998b3f1e532ff97d9365e031e2f1aff3
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-10-31 15:53:46 +0800

    新增刪除標籤刪除功能 & 新增使用者資料編輯

--- src/main/java/com/example/ion_nuxt_back/dto/users/request/editUserReqDTO.java
+++ src/main/java/com/example/ion_nuxt_back/dto/users/request/editUserReqDTO.java
@@ -1,22 +1,18 @@
-package com.example.ion_nuxt_back.dto.users.response;
+package com.example.ion_nuxt_back.dto.users.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Document(collection = "users")
 //  Lombok
 @Data //  auto getter, setter, toString, equals, hashCode
 @NoArgsConstructor // 無參數建構子
 @AllArgsConstructor // 全參數建構子
-public class UserInfoResDTO {
+
+public class editUserReqDTO {
     private String uuid;
-    private String account;
-    private String accessToken;
-    private String refreshToken;
     private String nick;
     private String infoImg;
 }
